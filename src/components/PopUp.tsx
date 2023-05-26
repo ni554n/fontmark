603b4f3e1f77474acac024c94e77dc4f3a67d7bc
Nissan Ahmed <aan007bd@gmail.com>
2023-05-26 21:30:45 +0600

    Initial commit w/ SolidJS ts-tailwindcss template

---
+++ src/components/PopUp.tsx
@@ -0,0 +1,5 @@
+import { Component } from "solid-js";
+
+export const PopUp: Component = () => {
+  return <div>Collections</div>;
+};
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+/* @refresh reload */
+import { render } from "solid-js/web";
+import "./index.css";
+
+import { PopUp } from "./components/PopUp";
+
+render(() => <PopUp />, document.getElementById("root"));
