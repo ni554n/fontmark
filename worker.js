3b2f5a950c3d8eeed97210000106524613bab828
Nissan Ahmed <aan007bd@gmail.com>
2023-06-14 18:06:02 +0600

    Add badge when the font is in a collection

--- worker.js
+++ worker.js
@@ -1,76 +1,19 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.bookmarks.search("https://fonts.google.com/share?", reset);
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  if (changeInfo.status !== "loading" || !changeInfo.url) return;
+
+  const paths = new URL(changeInfo.url).pathname.split("/");
+  const specimenIndex = paths.indexOf("specimen");
+
+  if (specimenIndex !== -1) {
+    chrome.bookmarks.search("https://fonts.google.com/share?", (bookmarks) => {
+      const fontName = paths[specimenIndex + 1];
+
+      for (const bookmark of bookmarks) {
+        if (bookmark.url?.includes(fontName)) {
+          chrome.action.setBadgeText({ tabId, text: "✓" });
+          break;
+        }
+      }
+    });
+  }
 });
-
-function reset(bookmarks) {
-  chrome.contextMenus.removeAll();
-  // chrome.action.disable();
-
-  // for (let i = 0; i < 6; i++) {
-  //   chrome.contextMenus.create({
-  //     id: `f${i}`,
-  //     title: `link #${i}`,
-  //     contexts: ["action"],
-  //     type: "checkbox",
-  //     documentUrlPatterns: ["*://fonts.google.com/*"],
-  //     checked: false,
-  //     visible: false,
-  //   });
-  // }
-
-  // for (let i = 0; i < 6; i++) {
-  //   chrome.contextMenus.create({
-  //     id: `m${i}`,
-  //     title: `Menu #${i}`,
-  //     contexts: ["action"],
-  //     type: "checkbox",
-  //     checked: false,
-  //   });
-  // }
-
-  // for (const { id, title } of bookmarks) {
-  //   chrome.contextMenus.create({
-  //     id,
-  //     title,
-  //     contexts: ["action"],
-  //     type: "checkbox",
-  //     checked: false,
-  //   });
-  // }
-
-  // // Page actions are disabled by default and enabled on select tabs
-
-  // // Clear all rules to ensure only our expected rules are set
-  // chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-  //   // Declare a rule to enable the action on example.com pages
-  //   let exampleRule = {
-  //     conditions: [
-  //       new chrome.declarativeContent.PageStateMatcher({
-  //         pageUrl: { hostSuffix: "fonts.google.com" },
-  //       }),
-  //     ],
-  //     actions: [new chrome.declarativeContent.ShowAction()],
-  //   };
-
-  //   // Finally, apply our new array of rules
-  //   let rules = [exampleRule];
-  //   chrome.declarativeContent.onPageChanged.addRules(rules);
-  // });
-}
-
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//   console.log(tabId, JSON.stringify(changeInfo), tab);
-// });
-
-chrome.runtime.onStartup.addListener(() => {});
-
-chrome.contextMenus.onClicked.addListener((menuItem, tab) => {
-
-});
-
-chrome.management.onEnabled.addListener(() => {});
-
-// chrome.action.onClicked.addListener((tab) => {
-//   // if (!(tab.url ?? "").startsWith("http")) return;
-//   // const tabUrl = new URL(tab.url);
-// });
